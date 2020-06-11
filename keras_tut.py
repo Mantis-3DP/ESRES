dc1eb420e0b4d328d501e52428af4675d56ef7c7
Mats <mantis3dp@posteo.de>
2020-06-11 16:33:28 +0200

    verwechselte Daten korrigiert

--- keras_tut.py
+++ keras_tut.py
@@ -46,6 +46,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 import scikitplot as skplt
 from keras.callbacks import TensorBoard
+import seaborn as sns
 
 
 
@@ -87,49 +88,58 @@ def create_custom_model(input_dim, output_dim, nodes, n=2, name='model'):
         model.add(Dense(output_dim, activation='softmax'))
 
         # Compile model
-        model.compile(Adam(lr=0.001), loss='categorical_crossentropy',
+        model.compile(Adam(lr=0.01), loss='categorical_crossentropy',
                       metrics=['accuracy'])
         return model
     return create_model
 
 
-#muss ich noch scalen
-fileloca_train = Path(__file__).parent / "data_sets/samleDataRefriRoom_transpose.csv"
-fileloca_test = Path(__file__).parent / "data_sets/validate_transpose.csv"
+
+fileloca_train = Path(__file__).parent / "data_sets/samleDataRefriRoom_transpose_new.csv"
+fileloca_test = Path(__file__).parent / "data_sets/test_empty.csv"
 dataset_train = pd.read_csv(fileloca_train)
-dataset_vali = pd.read_csv(fileloca_test)
+dataset_test = pd.read_csv(fileloca_test)
 
 dataset_train_values = dataset_train.values
-dataset_vali_values = dataset_vali.values
+dataset_test_values = dataset_test.values
 
 feature_names = list(dataset_train.columns[:-1])
 
-X = dataset_train_values[:, :17]
-y = dataset_train_values[:, 17]
+X = dataset_train_values[:, :-1]
+y = dataset_train_values[:, -1]
+X_test = dataset_test_values[:, :-1]
+y_test = dataset_test_values[:, -1]
 
 names = pd.Categorical(y).categories
 
 encoder = LabelEncoder()
+encoder_test = LabelEncoder()
 encoder.fit(y)
+encoder_test.fit(y_test)
 
 encoded_Y = encoder.transform(y)
+encoded_Y_test = encoder_test.transform(y_test)
 # One hot encoding
 enc = OneHotEncoder()
+enc_test = OneHotEncoder()
 Y = enc.fit_transform(y[:, np.newaxis]).toarray()
+Y_test = enc_test.fit_transform(y_test[:, np.newaxis]).toarray()
 
 train_samples, train_labels = shuffle(X, Y)
 scaler = MinMaxScaler(feature_range=(0, 1))
-X_scaled= scaler.fit_transform(train_samples)
 
-X_train, X_test, Y_train, Y_test = train_test_split(
-    X_scaled, Y, test_size=0.1, random_state=2)
+X_scaled= scaler.fit_transform(train_samples)
+X_scaled_test = scaler.fit_transform(X_test)
+
+X_train, X_val, Y_train, Y_val = train_test_split(
+    X_scaled, train_labels, test_size=0.1, random_state=2)
 
 n_features = X.shape[1]
 n_classes = Y.shape[1]
 
 
 models = [create_custom_model(n_features, n_classes, 10, n=i, name='model_{}'.format(i))
-          for i in range(1, 4)]
+          for i in range(1, 2)]
 
 for create_model in models:
     create_model().summary()
@@ -145,15 +155,26 @@ for create_model in models:
     #history_callback =\
     model.fit(X_train, Y_train,
                                  batch_size=1,
-                                 epochs=100,
+                                 epochs=3,
                                  #verbose=0,
-                                 validation_data=(X_test, Y_test),
-                                 callbacks=[cb])
-    score = model.evaluate(X_test, Y_test, verbose=0)
+                                 validation_data=(X_val, Y_val)
+              )
+    score = model.evaluate(
+        X_scaled_test,
+        Y_test,
+        verbose=0
+    )
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
+    predictions = model.predict(x=X_scaled_test, batch_size=1, verbose=0)
+    print(predictions)
+    rounded_predictions = np.argmax(predictions, axis=-1)
+    print('measures for test scenarios are:')
+    for i in rounded_predictions:
+        print(names[i])
 
-#  history_dict[model.name] = [history_callback, model]
+
+#history_dict[model.name] = [history_callback, model]
 
 
 
