c5389f140c8e36d987dc71038c5ca3bde5fa4f5b
Mats <mantis3dp@posteo.de>
2020-06-11 23:18:24 +0200

    added print of values

--- create_model.py
+++ create_model.py
@@ -9,7 +9,6 @@ from sklearn.utils import shuffle
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Activation, Dense
 from tensorflow.keras.optimizers import Adam
-import matplotlib.pyplot as plt  # doctest: +SKIP
 from sklearn.model_selection import train_test_split
 from keras.callbacks import TensorBoard
 import seaborn as sns
@@ -25,119 +24,122 @@ def create_custom_model(input_dim, output_dim, nodes, n=2, name='model'):
         model.add(Dense(output_dim, activation='softmax'))
 
         # Compile model
-        model.compile(Adam(lr=0.01), loss='categorical_crossentropy',
+        model.compile(Adam(lr=0.001), loss='categorical_crossentropy',
                       metrics=['accuracy'])
         return model
     return create_model
 
 
+if __name__=="__main__":
+    #change to import from .json
+    fileloca_train = Path(__file__).parent / "data_sets/samleDataRefriRoom_transpose_new.csv"
+    fileloca_test = Path(__file__).parent / "data_sets/test_transpose.csv"
+    dataset_train = pd.read_csv(fileloca_train)
+    dataset_test = pd.read_csv(fileloca_test)
 
-fileloca_train = Path(__file__).parent / "data_sets/samleDataRefriRoom_transpose_new.csv"
-fileloca_test = Path(__file__).parent / "data_sets/test_transpose.csv"
-dataset_train = pd.read_csv(fileloca_train)
-dataset_test = pd.read_csv(fileloca_test)
+    dataset_train_values = dataset_train.values
+    dataset_test_values = dataset_test.values
 
-dataset_train_values = dataset_train.values
-dataset_test_values = dataset_test.values
+    feature_names = list(dataset_train.columns[:-1])
 
-feature_names = list(dataset_train.columns[:-1])
+    X = dataset_train_values[:, :-1]
+    y = dataset_train_values[:, -1]
+    X_test = dataset_test_values[:, :-1]
+    y_test = dataset_test_values[:, -1]
 
-X = dataset_train_values[:, :-1]
-y = dataset_train_values[:, -1]
-X_test = dataset_test_values[:, :-1]
-y_test = dataset_test_values[:, -1]
+    names = pd.Categorical(y).categories
 
-names = pd.Categorical(y).categories
+    encoder = LabelEncoder()
+    encoder_test = LabelEncoder()
+    encoder.fit(y)
+    encoder_test.fit(y_test)
 
-encoder = LabelEncoder()
-encoder_test = LabelEncoder()
-encoder.fit(y)
-encoder_test.fit(y_test)
+    encoded_Y = encoder.transform(y)
+    encoded_Y_test = encoder_test.transform(y_test)
 
-encoded_Y = encoder.transform(y)
-encoded_Y_test = encoder_test.transform(y_test)
+    ######correlations
 
-######correlations
-
-corr_data = dataset_train
-corr_data['Measure'] = encoded_Y
-corr = corr_data.corr()
-heatmap = sns.heatmap(corr, xticklabels=list(dataset_train.columns[:]), yticklabels=list(dataset_train.columns[:]))
-plt.show()
-
-
-# One hot encoding
-enc = OneHotEncoder()
-enc_test = OneHotEncoder()
-Y = enc.fit_transform(y[:, np.newaxis]).toarray()
-Y_test = enc_test.fit_transform(y_test[:, np.newaxis]).toarray()
-
-train_samples, train_labels = shuffle(X, Y)
-scaler = MinMaxScaler(feature_range=(0, 1))
-
-X_scaled= scaler.fit_transform(train_samples)
-X_scaled_test = scaler.fit_transform(X_test)
-
-X_train, X_val, Y_train, Y_val = train_test_split(
-    X_scaled, train_labels, test_size=0.1, random_state=2)
-
-n_features = X.shape[1]
-n_classes = Y.shape[1]
+    corr_data = dataset_train
+    corr_data['Measure'] = encoded_Y
+    corr = corr_data.corr()
+    heatmap = sns.heatmap(corr, xticklabels=list(dataset_train.columns[:]), yticklabels=list(dataset_train.columns[:]))
+    plt.show()
 
 
-models = [create_custom_model(n_features, n_classes, 10, n=i, name='model_{}'.format(i))
-          for i in range(1, 3)]
+    # One hot encoding
+    enc = OneHotEncoder()
+    enc_test = OneHotEncoder()
+    Y = enc.fit_transform(y[:, np.newaxis]).toarray()
+    Y_test = enc_test.fit_transform(y_test[:, np.newaxis]).toarray()
 
-for create_model in models:
-    create_model().summary()
+    train_samples, train_labels = shuffle(X, Y)
+    scaler = MinMaxScaler(feature_range=(0, 1))
 
-history_dict = {}
+    X_scaled= scaler.fit_transform(train_samples)
+    X_scaled_test = scaler.fit_transform(X_test)
 
-# TensorBoard Callback
-cb = TensorBoard()
+    X_train, X_val, Y_train, Y_val = train_test_split(
+        X_scaled, train_labels, test_size=0.1, random_state=2)
 
-for create_model in models:
-    model = create_model()
-    print('Model name:', model.name)
-    #history_callback =\
-    model.fit(X_train, Y_train,
-                                 batch_size=1,
-                                 epochs=3,
-                                 #verbose=0,
-                                 validation_data=(X_val, Y_val)
-              )
-    score = model.evaluate(
-        X_scaled_test,
-        Y_test,
-        verbose=0
-    )
-    print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
-    predictions = model.predict(x=X_scaled_test, batch_size=1, verbose=0)
-    print(predictions)
-    rounded_predictions = np.argmax(predictions, axis=-1)
-    print('measures for test scenarios are:')
-    for i in rounded_predictions:
-        print(names[i])
+    n_features = X.shape[1]
+    n_classes = Y.shape[1]
 
 
-#history_dict[model.name] = [history_callback, model]
+    models = [create_custom_model(n_features, n_classes, 10, n=i, name='model_{}'.format(i))
+              for i in range(2, 3)]
 
-###### Save Keras Model
+    for create_model in models:
+        create_model().summary()
 
-model.save('cold_system_model.h5')
-print('model saved')
+    history_dict = {}
+
+    # TensorBoard Callback
+    cb = TensorBoard()
+
+    for create_model in models:
+        model = create_model()
+        print('Model name:', model.name)
+        #history_callback =\
+        model.fit(X_train, Y_train,
+                                     batch_size=1,
+                                     epochs=100,
+                                     #verbose=0,
+                                     validation_data=(X_val, Y_val)
+                  )
+        '''
+        score = model.evaluate(
+            X_scaled_test,
+            Y_test,
+            verbose=0
+        )
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
+        predictions = model.predict(x=X_scaled_test, batch_size=1, verbose=0)
+        print(predictions)
+        rounded_predictions = np.argmax(predictions, axis=-1)
+        print('measures for test scenarios are:')
+        for i in rounded_predictions:
+            print(names[i])
+        '''
+
+
+    #history_dict[model.name] = [history_callback, model]
+
+    ###### Save Keras Model
+
+    model.save(Path(__file__).parent / 'models/cold_system_model.h5')
+    print('model saved')
 
 
 
-#confusion matrix
-import seaborn as sns
+    #confusion matrix
 
-corr_data = dataset_train
-corr_data['Measure'] = encoded_Y
-corr = corr_data.corr()
-heatmap = sns.heatmap(corr, xticklabels=list(dataset_train.columns[:]), yticklabels=list(dataset_train.columns[:]))
-plt.show()
+
+    corr_data = dataset_train
+    corr_data['Measure'] = encoded_Y
+    corr = corr_data.corr()
+    heatmap = sns.heatmap(corr, xticklabels=list(dataset_train.columns[:]), yticklabels=list(dataset_train.columns[:]))
+    plt.show()
 
 
 
