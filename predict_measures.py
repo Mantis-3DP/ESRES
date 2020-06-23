85223a05397bf41ad19957711b82148f1733ff75
Mats <mantis3dp@posteo.de>
2020-06-23 15:42:52 +0200

    format prediction strings

--- predict_measures.py
+++ predict_measures.py
@@ -1,12 +1,9 @@
-
 from tensorflow import keras
 import numpy as np
 
 
 
 def predict_measure(modelloca, X_test):
-
-
     model = keras.models.load_model(modelloca)
     predictions = model.predict(x=X_test, batch_size=1)
     prediction_measure = np.round(predictions[:, 1], 2)
--- runme.py
+++ runme.py
@@ -5,23 +5,24 @@ from predict_measures import predict_measure
 from pathlib import Path
 import sys
 
-
 create_models: int = sys.argv[1]
 if create_models == 'create_models':
-    #create models
-    #link to data
+    # create models
+    # link to data
     fileloca_train = Path(__file__).parent / "datagen/samleData_vectoroutput.csv"
 
     num_measures = 8
 
-    #create_model.py needs a class with an __init__ instead of following
+    # create_model.py needs a class with an __init__ instead of following
 
-    feature_names, measure_names, X_train, X_val, Y_train, Y_val, n_features, n_classes = datapreprocess_train(fileloca_train,
-                                                                                                         num_measures)
+    feature_names, measure_names, X_train, X_val, Y_train, Y_val, n_features, n_classes = datapreprocess_train(
+        fileloca_train,
+        num_measures)
 
     model_number = 0
     for measure in measure_names:
-        create_all_models(X_train, X_val, Y_train[measure], Y_val[measure], n_features, n_classes[measure], model_number)
+        create_all_models(X_train, X_val, Y_train[measure], Y_val[measure], n_features, n_classes[measure],
+                          model_number)
         model_number += 1
 
 
@@ -32,16 +33,15 @@ elif create_models == 'test':
 
     num_measures = 8
     feature_names, measure_names, X_test, Y_test, n_features, n_classes = datapreprocess_test(fileloca_test,
-                                                                                            num_measures)
+                                                                                              num_measures)
 
     # looks redundant, is there a simpler way?
     predictions: dict = dict()
     for measure in measure_names:
         predictions[measure] = []
 
-
     for i in range(0, num_measures):
         modelloca = Path(__file__).parent / "models/cold_system_model_{}.h5".format(i)
         predictions[measure_names[i]] = predict_measure(modelloca, X_test)
 
-    show_predictions(predictions, measure_names, len(X_test))
+    show_predictions(predictions, measure_names, Y_test, len(X_test))
